9c02325dff4bd554e9dc3a1b58474de757b7d6cd
Katari Lokeswara rao <111894942+lokesh-katari@users.noreply.github.com>
2023-10-14 00:23:23 +0530

    added db

--- server/config/Db.js
+++ server/config/Db.js
@@ -1,9 +1,12 @@
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 
-const connectToMongo=()=>{
-    mongoose.connect(process.env.DB_URI).then(()=>{
-        console.log(" DB connection successfull ");
-    })
-    
-}
-module.exports=connectToMongo;
+const connectToMongo = () => {
+  mongoose
+    .connect(
+      "mongodb+srv://lokeshkatari921:aI09VQyUrAm78tQS@cluster0.uhgrpv4.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(() => {
+      console.log(" DB connection successfull ");
+    });
+};
+module.exports = connectToMongo;
